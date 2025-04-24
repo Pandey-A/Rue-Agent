caa0be1cd4e157c591f80e3e7d8bd585193f67ec
Ashutosh Pandey <155779632+Pandey-A@users.noreply.github.com>
2025-04-24 11:30:00 +0530

    chore: 20x10 extra tweaks on 2025-04-24 (#10)

--- frontend/electron/main.js
+++ frontend/electron/main.js
@@ -402,3 +402,4 @@ app.whenReady().then(createWindow);
 // extra20x10 tweak 2025-04-24 commit #7
 // extra20x10 tweak 2025-04-24 commit #8
 // extra20x10 tweak 2025-04-24 commit #9
+// extra20x10 tweak 2025-04-24 commit #10
