877bd481f0420099679bc23b0db1bcea542fa0ff
Ashutosh Pandey <155779632+Pandey-A@users.noreply.github.com>
2025-04-25 09:20:00 +0530

    chore: tweak main.js on Fri Apr 25 03:11:55 IST 2025 (#4)

--- frontend/electron/main.js
+++ frontend/electron/main.js
@@ -31,3 +31,4 @@ app.whenReady().then(createWindow);
 // tweak Fri Apr 25 03:11:55 IST 2025 commit #1
 // tweak Fri Apr 25 03:11:55 IST 2025 commit #2
 // tweak Fri Apr 25 03:11:55 IST 2025 commit #3
+// tweak Fri Apr 25 03:11:55 IST 2025 commit #4
