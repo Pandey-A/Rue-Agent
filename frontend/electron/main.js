c3ace7192766f8222a0fbe83d227a495d45bb094
Ashutosh Pandey <155779632+Pandey-A@users.noreply.github.com>
2025-07-03 12:26:00 +0530

    chore: jun30-jul8 tweaks on 2025-07-03 (#13)

--- frontend/electron/main.js
+++ frontend/electron/main.js
@@ -644,3 +644,4 @@ app.whenReady().then(createWindow);
 // jun30-jul8 tweak 2025-07-03 commit #10
 // jun30-jul8 tweak 2025-07-03 commit #11
 // jun30-jul8 tweak 2025-07-03 commit #12
+// jun30-jul8 tweak 2025-07-03 commit #13
